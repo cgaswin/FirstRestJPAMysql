3450cbe7cfa904ee5f856f8331676074a5bb5728
cgaswin <cgaswin333@gmail.com>
2024-07-23 16:04:27 +0530

    added put and delete controllers for product

--- src/main/java/com/iiht/rest/firstrestjpamysql/service/ProductService.java
+++ src/main/java/com/iiht/rest/firstrestjpamysql/service/ProductService.java
@@ -5,6 +5,9 @@ import com.iiht.rest.firstrestjpamysql.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductService {
 
@@ -13,5 +16,34 @@ public class ProductService {
 
     public Product save(Product product){
         return repository.save(product);
+    }
+
+    public List<Product> getAllProducts(){
+        return repository.findAll();
+    }
+
+    public Product getProductById(long productId){
+        Optional<Product> optional = repository.findById(productId);
+        return optional.orElse(null);
+    }
+
+    public Product updateProduct(long productId,Product product){
+        Optional<Product> optional = repository.findById(productId);
+        if(optional.isPresent()){
+            Product product1 = optional.get();
+            product.setProductId(product1.getProductId());
+            return repository.save(product);
+        }
+        return null;
+    }
+
+    public boolean deleteProduct(long productId){
+        Optional<Product> optional = repository.findById(productId);
+        if(optional.isPresent()){
+            Product product = optional.get();
+            repository.delete(product);
+            return true;
+        }
+        return false;
     }
 }

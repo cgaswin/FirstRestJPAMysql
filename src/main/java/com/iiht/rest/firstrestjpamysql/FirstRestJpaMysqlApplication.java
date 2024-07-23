5467ba3aad74b4917d117f619db838dc5bc5d9ad
cgaswin <cgaswin333@gmail.com>
2024-07-23 12:02:07 +0530

    Basic setup

---
+++ src/main/java/com/iiht/rest/firstrestjpamysql/FirstRestJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.iiht.rest.firstrestjpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestJpaMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstRestJpaMysqlApplication.class, args);
+    }
+
+}
